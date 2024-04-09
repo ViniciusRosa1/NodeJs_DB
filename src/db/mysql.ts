10913d334f2f8b6575e5c4a083cf91a60ac9dfc7
Vinicius <viniciusamarorosa5@gmail.com>
2024-04-09 10:56:14 -0300

    nodejs course using db

--- src/db/mysql.ts
+++ src/db/mysql.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 export const sequelize = new Sequelize(
     'teste',
     'root',
-    '', //DB password,
+    '', //DB password
     {
         dialect: 'mysql',
         port: parseInt(process.env.MYSQL_PORT as string)
